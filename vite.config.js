7cb321a3ab0eab7e03f18b3dac6d630e48c968f5
UMEH <emmanueldennise99@gmail.com>
2026-04-29 21:57:06 +0100

    allow railyway host

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173
-  }
+    port: 5173,
+  },
+  preview: {
+    host: "0.0.0.0",
+    port: Number(process.env.PORT || 8080),
+    allowedHosts: ["insighta-webportal-production.up.railway.app"],
+    // or use allowedHosts: true for quick unblock
+  },
 });
